c47e61d327d9cf81a5477a0607f5daa0e404657e
Jeemin Shim <jeemin.shim@gmail.com>
2020-01-08 01:11:16 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,10 +11,10 @@ puts say_hello(users_name)
 
 def help
   puts "I accept the following commands:"
-  puts "-help : displays this help message"
-  puts "list : displays a list of songs you can play"
-  puts "play : lets you choose a song to play"
-  puts "-exit"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this programt"
 end
 
 def list(songs)
